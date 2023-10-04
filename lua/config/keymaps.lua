103cae83a6fe692bef0cf8cfe46d1cdf92f2cf95
Mina Abadir <mina@abadir.email>
2023-10-04 17:04:16 -0400

    initial config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here This file is automatically loaded by
+-- lazyvim.config.init
+
+vim.keymap.set(
+  "n",
+  "<leader>gC",
+  require("telescope").extensions.git_worktree.create_git_worktree,
+  { noremap = true, silent = true, desc = "Create worktree" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>gw",
+  require("telescope").extensions.git_worktree.git_worktrees,
+  { noremap = true, silent = true, desc = "Create worktree" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
---
+++ lua/plugins/git-worktree.lua
@@ -0,0 +1,5 @@
+return {
+  "ThePrimeagen/git-worktree.nvim",
+  dependencies = { "telescope.nvim" },
+  config = function() end,
+}
