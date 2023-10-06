00f2071567b16174b58146a6b7d563ddfb5e8581
Mina Abadir <mina@abadir.email>
2023-10-05 20:21:33 -0400

    plugins and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,3 +23,5 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
+
+vim.keymap.set("n", "<leader>S", ":w<CR>", { noremap = true, silent = true, desc = "Write to file" })
